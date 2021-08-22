508b60f21a15e91217063b5104560bb206814546
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-08-22 16:01:13 +0530

    feat(3.3): Use next-urql with the server-side approach

--- src/lib/graphql/client.ts
+++ src/lib/graphql/client.ts
@@ -1,17 +1,43 @@
 import { NextPage } from 'next'
-import { dedupExchange, cacheExchange, fetchExchange } from '@urql/core'
-import { withUrqlClient } from 'next-urql'
+import { dedupExchange, cacheExchange, fetchExchange, ssrExchange } from '@urql/core'
+import { withUrqlClient, initUrqlClient } from 'next-urql'
+
 
 const UrqlProvider = (Page: NextPage) => withUrqlClient((ssrExchange) => ({
     // ...add your Client options here
-    url: process.env.GRAPHQL_ENDPOINT || 'http://localhost:3000/api/graphql',
+    url: process.env.NEXT_PUBLIC_GRAPHQL_API_URL,
     exchanges: [
         dedupExchange,
         cacheExchange,
         ssrExchange,
         fetchExchange
     ],
-  }))(Page)
+}))(Page)
+
+export const withUrqlProviderOnly = (Page: NextPage) => withUrqlClient(() => {
+    return ({
+        url: process.env.NEXT_PUBLIC_GRAPHQL_API_URL,
+    })
+}, { ssr: false })(Page)
+
+export const useUrqlClient = () => {
+    const ssrCache = ssrExchange({ isClient: false })
+
+    const client = initUrqlClient({
+      url: process.env.NEXT_PUBLIC_GRAPHQL_API_URL,
+      exchanges: [
+          dedupExchange,
+          cacheExchange,
+          ssrCache,
+          fetchExchange
+        ],
+    }, false)
+
+    return {
+        client,
+        ssrCache
+    }
+}
 
 
 export default UrqlProvider
--- src/pages/c/index.tsx
+++ src/pages/c/index.tsx
@@ -2,8 +2,16 @@ import { NextPage } from 'next'
 import styles from '~/styles/Home.module.css'
 import Layout from '~/components/Layout'
 import RecipeCategoryList from '~/components/recipes/RecipeCategoryList'
-import withUrqlClient from '~/lib/graphql/client'
+import { withUrqlProviderOnly, useUrqlClient } from '~/lib/graphql/client'
 
+const RECIPE_CATEGORIES_QUERY = `
+  query {
+    allRecipeCategories {
+        id
+        name
+    }
+  }
+`
 
 const RecipeCategoryIndexPage: NextPage = () => {
   return (
@@ -17,4 +25,19 @@ const RecipeCategoryIndexPage: NextPage = () => {
   )
 }
 
-export default withUrqlClient(RecipeCategoryIndexPage)
+export default withUrqlProviderOnly(RecipeCategoryIndexPage)
+
+export async function getServerSideProps(ctx) {
+    const { client, ssrCache } = useUrqlClient()
+
+    // This query is used to populate the cache for the query used on this page.
+    await client.query(RECIPE_CATEGORIES_QUERY).toPromise()
+
+    return {
+      props: {
+        // urqlState is a keyword here so withUrqlClient can pick it up.
+        urqlState: ssrCache.extractData(),
+      },
+    //   revalidate: 600,
+    };
+}
